93726c9a134b8744a45f2308554265f819b2db4c
Jaqueline <66227952+jaquelinesilfe@users.noreply.github.com>
2021-10-19 19:48:52 -0300

    Add project files.

---
+++ CalculatorContract/Services/IOnlinePaymentService.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace CalculatorContract.Services {
+    interface IOnlinePaymentService {
+
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+
+    }
+}
